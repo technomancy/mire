39327eb7c351bfc2a9c15081dcabd0c5c86cb033
Phil Hagelberg <technomancy@gmail.com>
2009-01-19 20:17:18 -0800

    Better elisp interaction.

--- mire.el
+++ mire.el
@@ -4,8 +4,18 @@
                 (or (buffer-file-name) load-file-name))
       swank-clojure-jar-path (concat mire-dir "jars/clojure.jar")
       swank-clojure-extra-classpaths (list (concat mire-dir "jars/clojure-contrib.jar")
-                                           mire-dir))
+                                           (concat mire-dir "src")))
 
 (slime)
 
 (find-file (concat mire-dir "src/mire.clj"))
+
+(defun mire ()
+  (interactive)
+  (if (get-buffer "*mire*")
+      (switch-to-buffer "*mire*")
+    (telnet "localhost" 3333)
+    (rename-buffer "*mire*")))
+
+(global-set-key (kbd "C-c m") 'mire)
+    
