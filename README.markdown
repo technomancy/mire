1c2e0d2006664cf2cd9f363cb6c0d524d5c75208
Phil Hagelberg <technomancy@gmail.com>
2009-01-25 21:54:20 -0800

    Trimmed it down to just the echo server.

--- README.markdown
+++ README.markdown
@@ -12,5 +12,11 @@ need a JVM on your system.
 To connect, simply telnet to port 3333, or hit M-x mire from within
 Emacs if you have used mire.el.
 
+## Note
+
+This branch is part of a series that builds up the full program
+step-by-step, but it's meant to be an incomplete project. See the
+master branch for the final product.
+
 Copyright (c) 2009 Phil Hagelberg
 Licensed under the same terms as Clojure.
--- src/mire.clj
+++ src/mire.clj
@@ -1,45 +1,17 @@
 #!/usr/bin/env clj
 
-(add-classpath (str "file://" (.getParent (java.io.File. *file*)) "/"))
-
 (ns mire
-  (:use [mire commands player rooms])
   (:use [clojure.contrib server-socket])
   (:import [java.io InputStreamReader OutputStreamWriter]
            [clojure.lang LineNumberingPushbackReader]))
 
 (def port 3333)
 
-(defn welcome []
-  (dosync (commute *players* conj {*name* *out*})
-          (commute (:inhabitants @*current-room*) conj *name*))
-  (println "Welcome to Mire, " *name* "\n")
-  (println (look))
-  (print prompt) (flush))
-
-(defn cleanup []
-  "Drop all inventory and remove player from room and player list."
-  (doseq [thing @*inventory*] (discard thing))
-  (dosync (commute *players* dissoc *name*)
-          (commute (:inhabitants @*current-room*)
-                   (partial remove #(= % *name*)))))
-
 (defn- mire-handle-client [ins outs]
   (binding [*in* (LineNumberingPushbackReader. (InputStreamReader. ins))
             *out* (OutputStreamWriter. outs)]
-    ;; bindings doesn't work sequentially, so we need to nest them
-    ;; otherwise the call to read-name uses the old value of *in*/*out*
-    (binding [*name* (read-name)
-              *inventory* (ref [])
-              *current-room* (ref (@mire.rooms/*rooms* :start))]
-      (welcome)
-      (try
-       (loop [input (read-line)]
-         (when input
-           (println (execute input))
-           (print prompt) (flush)
-           (recur (read-line))))
-       (finally (cleanup))))))
+    (loop [input (read-line)]
+      (println input)
+      (recur (read-line)))))
 
-(load-rooms (str (.getParent (java.io.File. *file*)) "/../data/rooms/"))
 (defonce *server* (create-server port mire-handle-client))
