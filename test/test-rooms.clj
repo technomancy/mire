c8231776046a6e51867ac10b73ba1a7d8cae7db6
Phil Hagelberg <technomancy@gmail.com>
2009-03-07 15:57:30 -0800

    Updated test suite for latest version.

--- test/test-rooms.clj
+++ test/test-rooms.clj
@@ -2,24 +2,18 @@
   (:use [mire player rooms])
   (:use [clojure.contrib test-is seq-utils]))
 
-(defn init []
-  (dosync (ref-set *rooms* {}))
-  (load-rooms (str (.getParent (java.io.File. *file*)) "/../data/rooms/")))
-
 (deftest test-load-rooms
-  (init)
   (doseq [name [:start :closet :hallway :promenade]]
-    (is (contains? @*rooms* name)))
-  (is (re-find #"promenade" (:desc (:promenade @*rooms*))))
-  (is (= :hallway (:west @(:exits (:promenade @*rooms*)))))
-  (is (includes? @(:items (:promenade @*rooms*)) :bunny))
-  (is (empty? @(:inhabitants (:promenade @*rooms*)))))
+    (is (contains? rooms name)))
+  (is (re-find #"promenade" (:desc (:promenade rooms))))
+  (is (= :hallway (:west @(:exits (:promenade rooms)))))
+  (is (includes? @(:items (:promenade rooms)) :bunny))
+  (is (empty? @(:inhabitants (:promenade rooms)))))
 
 (deftest test-room-contains?
-  (init)
-  (def closet (:closet @*rooms*))
-  (is (filter #(= % :keys) @(:items closet)))
-  (is (room-contains? closet "keys"))
-  (is (not (room-contains? closet "monkey"))))
+  (let [closet (:closet rooms)]
+    (is (not (empty? (filter #(= % :keys) @(:items closet)))))
+    (is (room-contains? closet "keys"))
+    (is (not (room-contains? closet "monkey")))))
 
-(run-tests)
+(load-rooms (str (.getParent (java.io.File. *file*)) "/../data/rooms/"))
