43ce1f4ee875d2fbba96805abf43d5aab1e7cdb8
Phil Hagelberg <technomancy@gmail.com>
2010-04-24 15:04:58 -0700

    Update test suite for Clojure 1.2.

--- src/mire/test/all.clj
+++
@@ -1,9 +0,0 @@
-(ns mire.test.all
-  (:use [mire.test commands])
-  (:use [mire.test rooms])
-  (:use [mire.test util])
-  (:use [clojure.contrib test-is]))
-
-(run-tests 'mire.test.commands)
-(run-tests 'mire.test.rooms)
-(run-tests 'mire.test.util)
